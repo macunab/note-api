92f718a1bd5fe4fb4487c6f87861a067e192ee63
Marshall <mn.acunab@gmail.com>
2022-09-22 18:09:05 -0300

    init express

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import dotenv from 'dotenv';
+const dotenvResult = dotenv.config();
+if(dotenvResult.error) {
+    throw dotenvResult.error;
+}
+import express, { Application } from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import passport from 'passport';
+
+const PORT: number = parseInt( process.env.PORT as string, 10);
+const app: Application = express();
+
+app.use(helmet);
+app.use(cors);
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Server run on port ${PORT}`)
+});
