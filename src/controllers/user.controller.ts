10231587a1d951e42f5567e9f89b020e2d07d31a
Marshall <mn.acunab@gmail.com>
2022-09-29 16:08:39 -0300

    create jwt

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from "express";
+import jwt from 'jsonwebtoken';
+
+class UserController {
+
+    createJwt(req: Request, res: Response) {
+        const JWT_SECRET_KEY = process.env.JWT_SECRET as string;
+        const user = req.user;
+        console.log(user);
+        if(user) {
+            const token = jwt.sign({ user },
+                JWT_SECRET_KEY,
+                { expiresIn: '1h'},
+                (err, token) => {
+                    if(err) {
+                        return res.status(400).json({
+                            ok: false,
+                            msg: err.message
+                        });
+                    }
+                    return res.status(200).json({
+                        token
+                    })
+                })
+        }
+    }
+}
+
+export default new UserController();
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,4 +1,6 @@
 import { Application, NextFunction, Request, Response } from "express";
+import passport from "passport";
+import userController from "../controllers/user.controller";
 import { CommonRoutesConfig } from "../helper/CommonRoutesConfig";
 
 export class UserRoute extends CommonRoutesConfig {
@@ -8,6 +10,12 @@ export class UserRoute extends CommonRoutesConfig {
     }
 
     configureRoutes(): Application {
+
+        this.app.route('/auth/google')
+            .get(passport.authenticate('sign-in-google', { scope: ['profile', 'email']}));
+        this.app.route('/auth/google/callback')
+            .get(passport.authenticate('sign-in-google', { session: false }),
+                userController.createJwt);    
 
         this.app
             .get('/users');
