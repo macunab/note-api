09fb2b6a0a7f1421baeffe63dd6c5ce7fa34f19a
Marshall <mn.acunab@gmail.com>
2022-09-29 16:24:19 -0300

    create category

--- src/controllers/category.controller.ts
+++ src/controllers/category.controller.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from "express";
+import { Category } from "../interfaces/category.interface";
 import categoryModel from "../models/category.model";
 
 class CategoryController {
 
     async findCategorysByUser(req: Request, res: Response) {
         const user = req.user;
+        console.log(req.user);
         if(!user) {
             return res.status(400).json({
                 ok: false,
@@ -18,16 +20,31 @@ class CategoryController {
                 msg: 'All documents found successfully',
                 data: categorys
             });
-        } catch(error) {
+        } catch(err) {
             res.status(400).json({
                 ok: false,
-                msg: `An error ocurred while trying find all documents, error: ${error}`
+                msg: `An error ocurred while trying find all documents, error: ${err}`
             });
         }
     }
 
     async createCategory(req: Request, res: Response) {
-        
+        const category: Category = req.body;
+        category.user = req.user!.user;
+        try{
+          const categoryQuery = new categoryModel(category);
+          await categoryQuery.save();
+          res.status(200).json({
+            ok: true,
+            msg: 'Category create successfully'
+          })
+
+        } catch(err) {
+            res.status(400).json({
+                ok: false,
+                msg: `An error ocurred while trying persist a new category, error: ${ err }`
+            })
+        }
     }
 }
 
--- src/routes/category.route.ts
+++ src/routes/category.route.ts
@@ -1,5 +1,8 @@
 import { Application, Request, Response, NextFunction } from "express";
+import { check } from "express-validator";
+import categoryController from "../controllers/category.controller";
 import { CommonRoutesConfig } from "../helper/CommonRoutesConfig";
+import validationFields from "../middlewares/validationFields";
 
 export class CategoryRoute extends CommonRoutesConfig {
     constructor(app: Application) {
@@ -8,9 +11,15 @@ export class CategoryRoute extends CommonRoutesConfig {
 
     configureRoutes(): Application {
         this.app.route('/categorys')
-            .get();
+            .get(categoryController.findCategorysByUser);
+
         this.app.route('/categorys')
-            .post();
+            .post( 
+                check('name', 'the name is required').not().isEmpty(),
+                check('color', 'the color is required').not().isEmpty(),
+                validationFields.verifyFieldsErrors,
+                categoryController.createCategory);
+
         this.app.route('/categorys/:id')
             .all((next: NextFunction) => {
                 next();
