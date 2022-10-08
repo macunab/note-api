82eba14485d363fa8dafb6c6b816725807e6d934
Marshall <mn.acunab@gmail.com>
2022-10-08 11:56:57 -0300

    jwt helper fix

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,39 +1,36 @@
 import { Request, Response } from "express";
-import jwt from 'jsonwebtoken';
 import userModel from "../models/user.model";
 import bcrypt from 'bcryptjs';
-import { User } from "../interfaces/user.interface";
 import jwtHelper from "../helper/jwtHelper";
 
 class UserController {
 
-    createJwt(req: Request, res: Response) {
-        const JWT_SECRET_KEY = process.env.JWT_SECRET as string;
+    async createJwt(req: Request, res: Response) {
         const user = req.user;
         console.log(user);
         if(user) {
-            const token = jwt.sign({ user },
-                JWT_SECRET_KEY,
-                { expiresIn: '1h'},
-                (err, token) => {
-                    if(err) {
-                        return res.status(400).json({
-                            ok: false,
-                            msg: err.message
-                        });
-                    }
-                    return res.status(200).json({
-                        ok: true,
-                        token
-                    })
+            try {
+                const token = await jwtHelper.jwtSign(user);
+                res.status(200).json({
+                    ok: true,
+                    token,
+                    msg: 'Token create successfully'
                 })
+            } catch(err) {
+                res.status(400).json({
+                    ok: false,
+                    msg: `An error ocurred while trying generated a jwt, error: ${ err }`
+                })
+            }
+
+            
         }
     }
 
     async authenticationWhitCredentials(req: Request, res: Response) {
         const { email, password }= req.body;
-        const JWT_SECRET_KEY = process.env.JWT_SECRET as string;
-        const user = await userModel.findOne({ email: email });
+        try {
+            const user = await userModel.findOne({ email: email });
         if(!user) {
             return res.status(400).json({
                 ok: false,
@@ -47,30 +44,20 @@ class UserController {
                 msg: 'The credentials do not correspond to an existing user'
             });
         }
-        const token = jwt.sign({ user }, 
-            JWT_SECRET_KEY,
-            { expiresIn: '1h'},
-            (err, token) => {
-                if(err) {
-                    return res.status(400).json({
-                        ok: false,
-                        msg: err.message
-                    });
-                }
-                return res.status(200).json({
-                    ok: true,
-                    token
-                })
+        const token = await jwtHelper.jwtSign(user);
+        res.status(200).json({
+            ok: true,
+            token,
+            msg: 'User authenticated successfully'
+        })
+        } catch(err) {
+            res.status(400).json({
+                ok: false,
+                msg: `An error ocurred while trying login, error: ${ err }`
             })
+        }
     }
 
-    /*
-        1) comprobar si el email no esta registrado.
-        2) encriptar el password.
-        3) guardar el usuario en la db.
-        4) generar el token.
-        5) retornar el token.
-    */
     async registerUserWithCredentials(req: Request, res: Response) {
         const { email, password } = req.body;
         try {
@@ -84,7 +71,7 @@ class UserController {
             const user = new userModel(req.body);
             const salt = bcrypt.genSaltSync();
             user.password = bcrypt.hashSync(password, salt);
-            const token = jwtHelper.jwtSign(user);
+            const token = await jwtHelper.jwtSign(user);
             await user.save();
             res.status(200).json({
                 ok: true,
--- src/helper/jwtHelper.ts
+++ src/helper/jwtHelper.ts
@@ -1,9 +1,8 @@
-import { User } from "../interfaces/user.interface";
 import jwt from 'jsonwebtoken';
 
 class JwtHelper {
 
-    jwtSign(user: User) {
+    jwtSign(user: any) {
         const payload = { user };
         const JWT_SECRET_KEY = process.env.JWT_SECRET as string;
         return new Promise(( resolve, reject ) => {
